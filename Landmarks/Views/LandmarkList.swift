6b7b204bfad9d5a1713a58ad034fa16537cb65a3
Osman Bowser <osmanbowser@gmail.com>
2022-07-25 17:25:30 -0500

    Create LandmarkList

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Osman Bowser on 7/25/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
