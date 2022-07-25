8168697068716f6a86bb5d2daad97d2a7c3364f5
Osman Bowser <osmanbowser@gmail.com>
2022-07-25 15:44:48 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Osman Bowser on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
