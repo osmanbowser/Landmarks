fc7661b968e78b25762364703e6736755bf6672e
Osman Bowser <osmanbowser@gmail.com>
2022-07-25 17:59:32 -0500

    Navigation between Link and Detail

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -17,16 +17,13 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             
+            Spacer()
         }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            LandmarkRow(landmark: landmarks[0])
-            LandmarkRow(landmark: landmarks[1])
-        }
-        .previewLayout(.fixed(width: 300, height: 70))
+        LandmarkRow(landmark: landmarks[0])
     }
 }
